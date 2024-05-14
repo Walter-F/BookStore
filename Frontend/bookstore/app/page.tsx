b18e5e457d460828ff6757578a4df44f3946c964
Walter-F <sva_work@inbox.ru>
2024-05-14 22:22:38 +0300

    Добавление изображения

--- Frontend/bookstore/app/page.tsx
+++ Frontend/bookstore/app/page.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <h1 className="welcome-label">Добро пожаловать в книжный магазин!</h1>;
+  return (
+    <div>
+      <h1 className="welcome-label">Добро пожаловать в книжный магазин!</h1>
+      <Image className="main-image"
+          src={"/assets/MainImage.jpg"}
+          alt="Main Image"
+          width={960}
+          height={640}
+      />
+    </div>
+    )
 }
